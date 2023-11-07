e3f8f4bc86637e2e30dc1ac22f1bce2dd208fba0
Bakeloh <bakelohnoel@gmail.com>
2023-11-07 12:06:41 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,64 +1,74 @@
+
 // Your  code here 
 
-// Define the base URL for your local server
 document.addEventListener("DOMContentLoaded", function () {
-    const baseUrl = "http://localhost:3000"; // Corrected variable name
+    
+    const baseUrl = "http://localhost:3000";
 
-    // Function to fetch movie details and update the movie details area
+
     function fetchMovieDetails(movieId) {
-        fetch(`${baseUrl}/movies/${movieId}`)
+
+        fetch(`${baseUrl}/films/${movieId}`)
             .then((response) => response.json())
             .then((data) => {
-                // ... Rest of the code remains the same ...
+
+                const posterElement = document.getElementById("poster");
+                const titleElement = document.getElementById("title");
+                const runtimeElement = document.getElementById("runtime");
+                const showtimeElement = document.getElementById("showtime");
+                const availableTicketsElement = document.getElementById("available-tickets");
+
+                posterElement.src = data.poster;
+                titleElement.textContent = data.title;
+                runtimeElement.textContent = `${data.runtime} minutes`;
+                showtimeElement.textContent = data.showtime;
+
+                const availableTickets = data.capacity - data.tickets_sold;
+                availableTicketsElement.textContent = availableTickets;
+
+
+                const buyTicketButton = document.getElementById("buy-ticket");
+                if (availableTickets === 0) {
+                    buyTicketButton.textContent = "Sold Out";
+                    buyTicketButton.disabled = true;
+                } else {
+                    buyTicketButton.textContent = "Buy Ticket";
+                    buyTicketButton.disabled = false;
+                }
             })
             .catch((error) => {
                 console.error("Error fetching movie details: ", error);
             });
     }
 
-    // Event listener for buying a ticket
+
     document.getElementById("buy-ticket").addEventListener("click", () => {
         if (currentMovieId !== null) {
-            // Make a PATCH request to update tickets_sold on the server
-            fetch(`${baseUrl}/movies/${currentMovieId}`, {
-                method: "PATCH",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({ tickets_sold: 1 }), // Assuming 1 ticket is purchased
-            })
-                .then((response) => response.json())
-                .then((data) => {
-                    // Update the movie details with the new data
-                    fetchMovieDetails(currentMovieId);
+
+            const availableTicketsElement = document.getElementById("available-tickets");
+            const availableTickets = parseInt(availableTicketsElement.textContent);
+            if (availableTickets > 0) {
+                r
+                fetch(`${baseUrl}/films/${currentMovieId}`, {
+                    method: "PATCH",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({ tickets_sold: 1 }),
                 })
-                .catch((error) => {
-                    console.error("Error buying a ticket: ", error);
-                });
+                    .then((response) => response.json())
+                    .then((data) => {
+
+                        fetchMovieDetails(currentMovieId);
+                    })
+                    .catch((error) => {
+                        console.error("Error buying a ticket: ", error);
+                    });
+            }
         }
     });
 
-    // Event listener for deleting a movie
-    document.getElementById("films").addEventListener("click", (event) => {
-        if (event.target.classList.contains("delete-movie")) {
-            const movieId = event.target.getAttribute("data-id");
 
-            // Make a DELETE request to delete the movie on the server
-            fetch(`${baseUrl}/movies/${movieId}`, {
-                method: "DELETE",
-            })
-                .then(() => {
-                    // Remove the movie item from the list
-                    event.target.parentElement.remove();
-                })
-                .catch((error) => {
-                    console.error("Error deleting the movie: ", error);
-                });
-        }
-    });
-
-    // Initially, load details of the first movie or set a default movie
-    const initialMovieId = 1; // You can set this to the desired default movie
+    const initialMovieId = 1;
     fetchMovieDetails(initialMovieId);
 });
-
